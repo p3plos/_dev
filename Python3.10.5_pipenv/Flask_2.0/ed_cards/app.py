194ef03d0889c40cbee516112c4f1f84f29a60c6
p3plos <peplosv@gmail.com>
2022-07-10 21:48:41 +0500

    Added icons and icon fonts in sources and connected postgres to the ed_cards wia Flask-SQLAlchemy

--- Python3.10.5_pipenv/Flask_2.0/ed_cards/app.py
+++ Python3.10.5_pipenv/Flask_2.0/ed_cards/app.py
@@ -1,6 +1,30 @@
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask_fontawesome import FontAwesome
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+fa = FontAwesome(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://l3ns:RDddy52postgres' \
+                                        '@localhost/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), unique=True)
+    create_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Users (id='{self.id}', name='{self.name}', " \
+               f"create_date='{self.create_date}')>"
 
 
 @app.route('/')
